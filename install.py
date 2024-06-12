e66a0b72cfc98ec5d1cc6122ec21ef20b890fff1
zombieyang <zombieyang@tencent.com>
2024-06-12 23:04:27 +0800

    string fix

--- install.py
+++ install.py
@@ -3,4 +3,4 @@ import launch
 
 package = 'python-socketio'
 if not launch.is_installed(package):
-    launch.run_pip(f"install {package}", f"sd-webui-infinite-image-browsing requirement: {package}")
+    launch.run_pip(f"install {package}", f"sd-ppp requirement: {package}")
