007e1d71486b66795b2ebd66a20fd789e7ff2dab
zombieyang <zombieyang@tencent.com>
2024-08-09 12:22:26 +0800

    prevent potential bug

--- install.py
+++ install.py
@@ -1,6 +1,9 @@
 #launch script for stable-diffusion
-import launch
+try:
+    import launch
 
-package = 'python-socketio'
-if not launch.is_installed(package):
-    launch.run_pip(f"install {package}", f"sd-ppp requirement: {package}")
+    package = 'python-socketio'
+    if not launch.is_installed(package):
+        launch.run_pip(f"install {package}", f"sd-ppp requirement: {package}")
+except ImportError:
+    pass
