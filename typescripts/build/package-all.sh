a3e3b09660ce9278f2eefa5ed2f7060e379aff06
zombieyang <zombieyang@tencent.com>
2025-04-15 20:53:55 +0800

    package script

--- typescripts/build/package-all.sh
+++ typescripts/build/package-all.sh
@@ -6,7 +6,7 @@ rm -rf sd-ppp_all.zip
 # Create a temporary directory structure
 mkdir -p temp_package/sd-ppp
 # Copy all required files/directories to the temporary structure
-cp -r ./javascript ./static ./sdppp_python __init__.py temp_package/sd-ppp/
+cp -r ./javascript ./static ./sdppp_python __init__.py NOTICE README.md LICENSE temp_package/sd-ppp/
 # Create the zip file from the temporary structure
 cd temp_package && zip -r ../sd-ppp_all.zip ./sd-ppp
 # Clean up the temporary directory
